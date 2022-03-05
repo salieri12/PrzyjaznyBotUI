86ee943661eb510392fad13115f1bac09b91dbe3
Tomasz Węgrzyn <tomekwegrzyn95@gmail.com>
2022-03-05 01:30:56 +0100

    Add table with fake data.

--- PrzyjaznyBotUI/src/app/app.module.ts
+++ PrzyjaznyBotUI/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BetsComponent } from './bets/bets.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { BetsComponent } from './bets/bets.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +23,14 @@ import { BetsComponent } from './bets/bets.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+     MatButtonModule,
+     MatFormFieldModule,
+     MatSelectModule,
+     MatTableModule,
+     MatPaginatorModule,
+     MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
